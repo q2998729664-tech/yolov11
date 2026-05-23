47c223d4424d00dceb7fedc90a903712b1595076
UltralyticsAssistant <web@ultralytics.com>
2026-05-23 04:04:09 +0000

    Auto-format by https://ultralytics.com/actions

--- mypredict.py
+++ mypredict.py
@@ -1,10 +1,11 @@
 from ultralytics import YOLO
+
 model = YOLO(r"C:\pythoncode\yolocode\ultralytics-8.3.163\results\yolo11m4\weights\best.pt")
 model.predict(
     source=r"C:\Users\29987\Downloads\QQ2026217-225016.mp4",
-    save = False,
-    show = True,
-    conf = 0.05,
-    iou = 0.9,
-    max_det = 99999,
+    save=False,
+    show=True,
+    conf=0.05,
+    iou=0.9,
+    max_det=99999,
 )
